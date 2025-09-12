7c664ef03288432043d1a6518b4e0c5d9dd1db6e
radhe8227 <shyamradhe45554@gmail.com>
2025-09-12 17:49:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,10 @@ connectDB();
 const app = express();
 app.use(express.json());
 
+// Routes
+app.use("/api/auth", require("./routes/authRoutes"));
+
+
 app.get("/", (req, res) => {
   res.send("Campus EventHub Backend Running 🚀");
 });
