75a4c05a25384676a56450613667e0ba8cea54e7
radhe8227 <shyamradhe45554@gmail.com>
2025-09-12 19:46:36 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
   const token = req.header("Authorization")?.replace("Bearer ", "");
-  if (!token) return res.status(401).json({ message: "Token नहीं मिला" });
+  if (!token) return res.status(401).json({ message: "Token " });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -10,10 +10,11 @@ const authMiddleware = (req, res, next) => {
     next();
   } catch (error) {
     if (error.name === "TokenExpiredError") {
-      return res.status(401).json({ message: "Token Expired हो गया है" });
+      return res.status(401).json({ message: "Token Expired " });
     }
     res.status(401).json({ message: "Invalid Token" });
   }
 };
 
 module.exports = authMiddleware;
+
